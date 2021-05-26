a4983b1be456da17ff831dd7b9a3ebc83913ad9d
erayyayla <eray_been@hotmail.com>
2021-05-26 03:10:27 +0300

    Reorder Level has been added to product details,visual editing has been done and the final version has been released

--- src/views/ProductDetails.js
+++ src/views/ProductDetails.js
@@ -2,7 +2,6 @@ import React, {useState,useEffect} from "react";
 import { ScrollView } from 'react-native';
 import { View,StyleSheet, Text } from "react-native";
 import { createStackNavigator } from 'react-navigation-stack';
-// import props from 'prop-types';
 
 
 export default function ProductDetails({navigation}) {
@@ -10,24 +9,31 @@ export default function ProductDetails({navigation}) {
 
     const styles2 = StyleSheet.create({
         text: {
-        fontFamily: 'Times New Roman',
+        fontFamily: 'Helvetica',
         fontSize: 20,
-        color: 'blue',
-        fontWeight: 'bold',
-        fontStyle: 'italic',
+        color: '#191970',
         textAlign: 'center'
-        }     
+        },
+        text2: {
+        fontFamily: 'Helvetica',
+        fontSize: 40,
+        color: '#663399',
+        fontWeight: 'bold',
+        textAlign: 'center'
+        }    
         });
 
     return (
         <ScrollView>
             {
-              <View>          
-                 <Text style={styles2.text}> Id: {navigation.getParam('id', 'Not Found!')}</Text>
-                 <Text style={styles2.text}> Supplier Id: {navigation.getParam('supplierId', 'Not Found!')}</Text>
-                 <Text style={styles2.text}> Catergory Id: {navigation.getParam('categoryId', 'Not Found!')}</Text>
+              <View>
+                 <Text style={styles2.text2}> Details of the Selected Product </Text>          
+                 <Text style={styles2.text}> Product ID: {navigation.getParam('id', 'Not Found!')}</Text>
+                 <Text style={styles2.text}> Supplier ID: {navigation.getParam('supplierId', 'Not Found!')}</Text>
+                 <Text style={styles2.text}> Catergory ID: {navigation.getParam('categoryId', 'Not Found!')}</Text>
                  <Text style={styles2.text}> Units In Stock: {navigation.getParam('unitsInStock', 'Not Found!')}</Text>
                  <Text style={styles2.text}> Units On Order: {navigation.getParam('unitsOnOrder', 'Not Found!')}</Text>
+                 <Text style={styles2.text}> Reorder Level: {navigation.getParam('reorderLevel', 'Not Found!')}</Text>
                </View>              
             }
         </ScrollView>
