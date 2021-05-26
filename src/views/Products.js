dfd2c6a240404703595ff248b1793513a47920dc
erayyayla <eray_been@hotmail.com>
2021-05-26 03:08:47 +0300

    Visual Editing has been done and the final version has been released

--- src/views/Products.js
+++ src/views/Products.js
@@ -5,7 +5,7 @@ import { ScrollView,TouchableOpacity } from 'react-native';
 import { createStackNavigator} from 'react-navigation-stack';
 import props from 'prop-types';
 
-export default function GetProduct({navigation}) {
+export default function GetProducts({navigation}) {
   
     const [products, setProducts] = useState([]);
     const getProducts = () =>
@@ -29,42 +29,42 @@ export default function GetProduct({navigation}) {
         .then(() => { getProducts(); })
       }
 
-    function moveProductDetails(id,supplierId,categoryId,unitsInStock,unitsOnOrder){
-      console.log(unitsOnOrder)
+    function movetoProductDetails(id,supplierId,categoryId,unitsInStock,unitsOnOrder,reorderLevel){
       navigation.navigate('ProductDetails', {
       id:id,
       supplierId:supplierId,
       categoryId:categoryId,
       unitsInStock:unitsInStock,
-      unitsOnOrder:unitsOnOrder});
+      unitsOnOrder:unitsOnOrder,
+      reorderLevel:reorderLevel});
     }
 
     const styles = StyleSheet.create({
         text: {
-        fontFamily: 'Times New Roman',
-        fontSize: 20,
-        color: 'blue',
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-        textAlign: 'center'
+         fontFamily: 'Helvetica',
+         fontSize: 20,
+         color: '#f5fffa',
+         fontWeight: 'bold',
+         textAlign: 'center'
         },
 
         bar: {
-            backgroundColor:'yellow',
-            margin:10,
-            borderWidth: 5,
-            borderTopLeftRadius: 40,
-            borderTopRightRadius: 40,
-            borderBottomLeftRadius: 40,
-            borderBottomRightRadius: 40         
+          backgroundColor:'#663399',
+          margin:10,
+          borderWidth: 5,
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+          borderBottomLeftRadius: 40,
+          borderBottomRightRadius: 40         
         },
         Button1: {
-            height:50,
-            width:200,
-            marginTop:2,
-            padding:10,
-            marginBottom:10,
-            alignSelf: 'center'
+          fontFamily: 'Helvetica',
+          height:50,
+          width:200,
+          marginTop:2,
+          padding:10,
+          marginBottom:10,
+          alignSelf: 'center'
         }
         
         });
@@ -75,14 +75,15 @@ export default function GetProduct({navigation}) {
             {
               
                 products.map((item) => (
-                    <TouchableOpacity style={styles.bar} onPress={() => moveProductDetails                          (item.id,
+                    <TouchableOpacity style={styles.bar} onPress={() => movetoProductDetails(item.id,
                     item.supplierId,
                     item.categoryId,
                     item.unitsInStock,
-                    item.unitsOnOrder)}>
+                    item.unitsOnOrder,
+                    item.reorderLevel)}>
                         <Text style={styles.text}> Product Name: {item.name}</Text>
                         <Text style={styles.text}> Quantity Per Unit: {item.quantityPerUnit}</Text>
-                        <Text style={styles.text}> Unit Price: {item.unitPrice + '$'}</Text>
+                        <Text style={styles.text}> Unit Price: {item.unitPrice + ' $'}</Text>
                         <Button title="Delete Product" style={styles.Button1} onPress={() => deleteProduct(item.id)}></Button>
                     </TouchableOpacity>
                 ))
