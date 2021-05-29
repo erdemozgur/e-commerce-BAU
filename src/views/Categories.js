b3a6b26963ff2bb8c4f5d12d9829e0ad154c3373
semihozsoy <semih__ozsoy@hotmail.com>
2021-05-29 23:33:59 +0300

    request delete item from http delete function

--- src/views/Categories.js
+++ src/views/Categories.js
@@ -1,31 +1,44 @@
 import React,{useState,useEffect} from "react";
-import { View, Text,FlatList,StyleSheet,ActivityIndicator,SafeAreaView } from "react-native";
-
+import { View, Text,FlatList,StyleSheet,ActivityIndicator,SafeAreaView,Button } from "react-native";
+import {Icon } from "react-native-elements";
 
 
 export default function Categories(){
 
-    const [data,setData]=useState({});
+    const [data,setData]=useState([]);
     const categoriesUrl ="https://northwind.vercel.app/api/categories"
-    const [name,setName]=useState([]);
-    const [description,setDescription]=useState([]);
-    const [id,setID]=useState([]);
+
+    
+    const getApi = () => {
+        fetch(categoriesUrl)
+        .then((response)=>response.json())
+        .then((json)=>{
+            setData(json);
+        })
+         .catch((error)=>console.error(error))   
+    
+    }
 
     useEffect(()=>{
-    fetch(categoriesUrl)
-    .then((response)=>response.json())
-    .then((json)=>{
-        setData(json);
-    })
-     .catch((error)=>console.error(error))   
-    });
+        getApi();
+    },[]);
+
     
-    
+
+    const deleteApi=(id)=>{
+       
+        fetch("https://northwind.vercel.app/api/categories/"+id,{
+            method: 'DELETE'
+        })
+        .then((response)=>response.json())
+        .then((data) =>{
+            getApi();
+        })      
+    }
+   
 
     return(
         <SafeAreaView>
-
-     
         <View style={styles.container}>
             <FlatList 
             data={data} keyExtractor={({id},index)=>id}
@@ -33,12 +46,17 @@ export default function Categories(){
             <View style={{paddingBottom:10}}>
             <Text style={styles.categoriesText}>
             {item.id}.{item.description},{item.name}
-            </Text>  
+            <Button onPress={()=> deleteApi(item.id)}></Button>
+            </Text> 
+             
             </View>
             )} >
             
             </FlatList>
         </View>
+
+                
+
         </SafeAreaView>
     );
 
